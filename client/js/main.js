0611201d0f4138a205a7d71a15dd49b677aa0b90
Noah Huppert <noahhuppert@gmail.com>
2017-01-02 14:26:06 -0500

    Started client side cache db

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,62 @@
+"use strict";
+
+class Database {
+    constructor() {
+        // DB name
+        this.name = "SquadUpDatabase";
+
+        // Define schema
+        this.db = new Dexie(this.name);
+        this.opened = false;
+
+        db.version(1).stores({
+            users: "id,firstName,lastName,email,profilePictureUrl,createdAt,updatedAt"
+        });
+
+        // Open db
+        // This open method can be called in the constructor because Mixie automatically holds all
+        // db operations until after open is done (Relevant docs: https://github.com/dfahlander/Dexie.js/wiki/Dexie.open())
+        db.open().catch(err => {
+            console.error(`Failed to open database: "${this.name}", err: ${err.stack || err}`);
+        });
+    }
+
+    /**
+     * Returns database
+     * @returns {Dexie} Database
+     */
+    get () {
+        return this.db;
+    }
+}
+
+/**
+ * A base class for data in the application. Provides base methods for saving data and fetching
+ * updates from the server.
+ */
+class Model {
+    constructor(state, name) {
+        this.state = state;
+
+        // Model name, must be the same as the IDB store name
+        this.name = name;
+
+        // Lazy loaded model data
+        this.data = undefined;
+    }
+}
+
+/**
+ * Application state, sectioned off into different areas of responsibility (ex., authentication, database)
+ * to maintain organization.
+ * Basically a masthead.
+ */
+class State {
+    constructor () {
+        this.db = new Database();
+    }
+}
+
 Polymer({
     // Element namhttp://jinzhu.me/gorm/e
     is: "x-app",
@@ -9,6 +68,10 @@ Polymer({
 
         this.listen(this.$["sign-in-btn"], "google-signin-success", "onGSignInSuccess");
     },
+
+    // Application state
+    // Holds all data and logic of the app
+    state: new State(),
 
     // Event handlers
     onGSignInSuccess: function(a, b, c) {
